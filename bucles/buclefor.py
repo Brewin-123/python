97a98d3793bd5513538004b3c5cd5b68925398b8
Brewin Sanchez <herrerabrewin@gmail.com>
2025-07-07 09:27:21 -0500

    Bucles while y for

---
+++ bucles/buclefor.py
@@ -0,0 +1,14 @@
+# lista = [1,2,3,4,5,6,7,8,9,10]
+
+# for i in lista:
+#     print(i, lista)
+
+# lista = [1,2,3,4,5,6,7,8,9,True,"Brewin","Python"]
+
+# for i in lista:
+#     print(f"Los elementos: {lista}")
+
+
+# for i in range(20):
+#     print(f"Elemento: {i}")
+
---
+++ bucles/buclewhile.py
@@ -0,0 +1,34 @@
+# i = 0
+
+# while i<=10:
+#     print("Hola mundo", i)
+#     i+=1
+# else:
+#     print("Finalizó el programa")
+
+
+# edad = int(input("Ingrese su edad: "))
+
+# while edad <0:
+#     print("Has introducido una edad incorrecta, vuelve a intentarlo")
+#     edad = int(input("Ingrese su edad: "))
+# else:
+#     print("Muchas gracias, finalizó el programa.")
+
+
+# i = 0
+
+# while i<=5:
+#     print(i)
+#     i+=1
+#     if (i==3):
+#         break
+
+# i = 0
+
+# while i<=5:
+#     print(i)
+#     i+=1
+#     if (i==3):
+#         print("Aquí es ==3")
+#         continue
