2a0121484bca8aeacdfa0d676dbbcabc4e5496d3
Brewin Sanchez <herrerabrewin@gmail.com>
2025-07-09 11:26:36 -0500

    aprendiendo colecciones

---
+++ colecciones/conjuntos.py
@@ -0,0 +1,26 @@
+# conjunto = {1,3}
+# print(conjunto)
+
+# #AGREGAR ELEMENTO
+
+# # conjunto.add(2)
+# # print(conjunto)
+
+
+# #AGREGAR LISTA DE ELEMENTOS
+
+# conjunto.update([4,5,6])
+# print(conjunto)
+
+
+# #ELIMINAR ELEMENTOS
+# conjunto.discard(4)
+# print(conjunto)
+
+# conjunto.remove(5)
+# print(conjunto)
+
+
+# #ELIMINAR EL CONJUNTO
+# conjunto.clear()
+# print(conjunto)
---
+++ colecciones/tuplas.py
@@ -0,0 +1,5 @@
+# tupla = ('Hola mundo', 2, 4.5, ['Brewin', 'María', 'Pedro'])
+
+# print(tupla[0:])
+
+#Nota: las Tuplas, a diferencia de las listas, no se pueden modificar.
