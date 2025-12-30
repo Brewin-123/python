7b228d996afbcf1fabb998fce47a8f1f15b13feb
Brewin Sanchez <herrerabrewin@gmail.com>
2025-12-30 08:41:47 -0500

    parte del día 2 con variables, floats e integers

---
+++ DÍA 2/integers-y-floats.py
@@ -0,0 +1,48 @@
+mi_numero  = 1 + 3
+print(mi_numero)
+
+print(type(mi_numero))
+
+mi_num = 2.8
+print(mi_num)
+print(type(mi_num))
+
+print(mi_num + mi_numero)
+
+edad = input("Digite su edad: ")
+print("Tu edad es " + edad)
+
+
+
+#Declara una variable numérica llamada num_entero que contenga un valor de tipo integer de tu elección.
+
+#Imprime el tipo de dato de dicha variable.
+
+num_entero = 24
+print(type(num_entero))
+
+
+#Declara una variable numérica llamada num_decimal que contenga un valor de tipo float de tu elección.
+
+#Imprime el tipo de dato de dicha variable.
+
+num_decimal = 2.9
+print(type(num_decimal))
+
+
+#¿De qué tipo es el resultado de la suma de 7.5 + 2.5? Genera el código para verificarlo.
+
+#Para ello, crea dos variables:
+
+#num1 = 7.5
+
+#num2 = 2.5
+
+#A continuación, muestra en pantalla el tipo de dato que resulta de la suma de ambos números.
+
+#Realiza el mismo ejercicio en PyCharm para ver el resultado. ¿Coindice con lo que esperabas?
+
+num1 = 7.5
+num2 = 2.5
+
+print(type(num1 + num2))
---
+++ DÍA 2/variables.py
@@ -0,0 +1,47 @@
+name = "Juan"
+print(name)
+
+name = "Laura"
+print(name)
+
+edad = 30
+edad2 = 15
+print(edad + edad2)
+
+nombre = input("Dime tu nombre: ")
+apellido = input("Dime tu apellido: ")
+nombreCompleto = nombre + " " + apellido
+
+print(nombreCompleto)
+
+
+#Declara dos variables, llamadas nombre y edad.
+#Asigna a la variable nombre el valor "Tony Soprano", y a la edad, el valor 51.
+
+nombre = "Tony Soprano"
+edad = 51
+
+
+#Crea tres variables:
+
+#nombre
+
+#apellido
+
+#nombrecompleto
+
+#A nombre, asígnale el valor "Julia", y en apellido, asigna el valor "Roberts". Finalmente, construye la variable nombrecompleto concatenando las variables (recuerda sumar un espacio intermedio).
+
+nombre = "Julia"
+apellido = "Roberts"
+nombrecompleto = nombre + " " + apellido
+
+
+#Declara la variable curso, asígnale el valor "Python", y muestra en pantalla la frase:
+
+#Estás tomando un curso de curso
+
+#Para ello deberás concatenar la primera parte de la frase con el valor que asumirá la variable. Recuerda agregar un espacio antes de concatenar la variable al resto del texto.
+
+curso = "Python"
+print("Estás tomando un curso de " + curso)
